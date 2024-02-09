581591ce4235b9d4bb6a0e524be683c2449ca6f8
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2024-02-09 11:44:39 +0200

    done-5

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -1,23 +1,28 @@
+import { useSearchParams } from 'react-router-dom';
 import css from './MovieForm.module.css';
 import { toast } from 'react-toastify';
+import { useEffect, useRef } from 'react';
 
 export default function MovieForm({ onSubmit }) {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get('query');
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.value = query;
+  });
+
   const formSubmit = e => {
     e.preventDefault();
     const { value } = e.currentTarget.movie;
     const validatedValue = value.trim();
-    reset(e);
     if (validatedValue === '') return toast.warn('Please fill empty field');
     onSubmit(validatedValue);
   };
 
-  const reset = e => {
-    e.currentTarget.movie.value = '';
-  };
-
   return (
     <form className={css.movieForm} onSubmit={formSubmit}>
-      <input type="text" name="movie"></input>
+      <input type="text" name="movie" ref={inputRef}></input>
       <button type="submit">Search</button>
     </form>
   );
